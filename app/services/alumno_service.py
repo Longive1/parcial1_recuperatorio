ecea5c739c2ae16e8a7b889be95e9f3de4b15045
Federico Sosa <federicomsosa@gmail.com>
2025-11-06 07:26:59 -0300

    Fix: Ejercicio N°4 Parte 2. (#4)

--- app/services/alumno_service.py
+++ app/services/alumno_service.py
@@ -25,25 +25,33 @@ class AlumnoService(BaseService):
         alumno_existente.especialidad_id = data.especialidad_id
         return self.repository.actualizar(alumno_existente)
         
-    def generar_certificado_alumno_regular(self, id: int,tipo: str)-> BytesIO:
-        alumno = self.repository.buscar_por_id(id)
+    def generar_certificado_alumno_regular(self, id: int, tipo: str) -> BytesIO:
+
+        alumno = self.alumno_repository.buscar_por_id(id)
         if not alumno:
             return None
         
-        context = AlumnoService.__obteneralumno(alumno)
+        context = self.__obteneralumno(alumno)
         documento = obtener_tipo_documento(tipo)
         if not documento:
             return None
-        
+        plantillas = {
+            'pdf': 'certificado_pdf',       # Usa la plantilla HTML
+            'odt': 'certificado_plantilla',  # Usa la plantilla ODT
+            'docx': 'certificado_plantilla' # Usa la plantilla DOCX
+        }
+        plantilla_seleccionada = plantillas.get(tipo)
+        if not plantilla_seleccionada:
+            return None
         return documento.generar(
-            carpeta='certificado',
-            plantilla='certificado_pdf',
+            carpeta='template/certificado', 
+            plantilla=plantilla_seleccionada, 
             context=context
- )
+        )
     
     def __obtener_fechaactual(self):
         fecha_actual = datetime.datetime.now()
-        fecha_str = fecha_actual.strftime('%d de %B de %Y')
+        fecha_str = fecha_actual.strftime('%d de %M de %Y')
         return fecha_str
     
     def __obteneralumno(self, alumno: Alumno) -> dict:
